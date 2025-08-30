0a1bca5cf031c265d27d818c0dcc1c611524d515
rivanghafara <rgdevrianza@gmail.com>
2025-08-30 14:42:57 +0700

    add github action configuration

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -5,12 +5,16 @@ export class ProductPage extends BasePage{
     readonly productHeader: Locator;
     readonly addItemButton: Locator;
     readonly cartBadge: Locator;
+    readonly listOfItem: Locator;
+    readonly prodoctName: Locator;
 
     constructor(page: Page) {
         super(page);
         this.productHeader = page.locator("[class='header_secondary_container']");
         this.addItemButton = page.locator('.btn_inventory');
         this.cartBadge = page.locator(".shopping_cart_badge");
+        this.listOfItem = page.locator(".inventory_list")
+        this.prodoctName = page.locator(".inventory_item_name")
     }
 
     async validateProductPageIsLoaded() {
@@ -20,7 +24,7 @@ export class ProductPage extends BasePage{
     async addNItems(n: number) {
         for (let i:number = 0; i < n; i++) {
             await this.addItemButton.nth(i).click()
-            await expect(this.addItemButton.nth(i)).toHaveText("Remove")
+            await expect(this.addItemButton.nth(i)).toHaveText("Remove", { ignoreCase: true })
         }
     }
 
@@ -29,7 +33,7 @@ export class ProductPage extends BasePage{
         const randomItem: number = Math.floor(Math.random() * totalItems);
 
         await this.addItemButton.nth(randomItem).filter({hasText: "ADD TO CART"}).click()
-        await expect(this.addItemButton.nth(randomItem)).toHaveText("Remove")
+        await expect(this.addItemButton.nth(randomItem)).toHaveText("Remove", { ignoreCase: true })
     }
 
     async goToCart() {
--- tests/Product.spec.ts
+++ tests/Product.spec.ts
@@ -1,68 +1,66 @@
-import {ProductPage} from "../pages/ProductPage";
-import {LoginPage} from "../pages/LoginPage";
-import {expect, test} from "@playwright/test";
-import {CartPage} from "../pages/CartPage";
-import {CheckoutPage} from "../pages/CheckoutPage";
-import {OverviewPage} from "../pages/OverviewPage";
-import {CheckoutCompletePage} from "../pages/CheckoutCompletePage";
-
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductPage } from "../pages/ProductPage";
+import { CartPage } from "../pages/CartPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
+import { OverviewPage } from "../pages/OverviewPage";
+import { CheckoutCompletePage } from "../pages/CheckoutCompletePage";
 
 let loginPage: LoginPage;
 let productPage: ProductPage;
 let cartPage: CartPage;
 let checkoutPage: CheckoutPage;
 let overviewPage: OverviewPage;
-let checkoutCompletePage: CheckoutCompletePage
+let checkoutCompletePage: CheckoutCompletePage;
 
-test.beforeEach(async ({page}) => {
-    loginPage = new LoginPage(page)
-    productPage = new ProductPage(page)
-    cartPage = new CartPage(page)
-    checkoutPage = new CheckoutPage(page)
-    overviewPage = new OverviewPage(page)
-    checkoutCompletePage = new CheckoutCompletePage(page)
+test.describe("Add product", () => {
+	test.beforeEach(async ({ page }) => {
+		loginPage = new LoginPage(page);
+		productPage = new ProductPage(page);
+		cartPage = new CartPage(page);
+		checkoutPage = new CheckoutPage(page);
+		overviewPage = new OverviewPage(page);
+		checkoutCompletePage = new CheckoutCompletePage(page);
 
-    await loginPage.goto()
-    await loginPage.validateLoginPageIsLoaded()
-    await loginPage.login('standard_user', 'secret_sauce')
-    await expect(page).toHaveURL(/.*\/inventory\.html/);
-})
+		await loginPage.goto();
+		await loginPage.validateLoginPageIsLoaded();
+		await loginPage.login("standard_user", "secret_sauce");
+		await expect(page).toHaveURL(/.*\/inventory\.html/);
+	});
 
-test('Add 2 items to cart', async ({ page }) => {
-    const numOfItems: number = 2;
+	test.afterEach(async({page}) => {
+		await page.evaluate(() => {
+			localStorage.clear();
+			sessionStorage.clear();
+		});
+	});
 
-    await productPage.validateProductPageIsLoaded()
-    await productPage.addNItems(numOfItems)
-    await expect(productPage.cartBadge).toHaveText(numOfItems.toString());
-})
+	[{ numOfItems: 2 }, { numOfItems: 3 }, { numOfItems: 4 }].forEach(
+		({ numOfItems }) => {
+			test(`Add ${numOfItems} items`, async () => {
+				await productPage.validateProductPageIsLoaded();
+				await productPage.addNItems(numOfItems);
+				await expect(productPage.cartBadge).toHaveText(numOfItems.toString());
+			});
+		}
+	);
 
-test('Add 3 items to cart', async ({ page }) => {
-    const numOfItems: number = 3;
+	test("Successfully making a purchase without any item", async ({ page }) => {
+		await productPage.addRandomItems();
+		await productPage.goToCart();
+		await expect(page).toHaveURL(/.*\/cart\.html/);
+		await cartPage.removeFirstItem();
+		await cartPage.checkingOut();
+		await expect(page).toHaveURL(/.*\/checkout-step-one\.html/);
+		await checkoutPage.inputInformation("John", "Maven", "12345");
+		await expect(page).toHaveURL(/.*\/checkout-step-two\.html/);
+		await expect(overviewPage.totalSum).toContainText("$0.00");
+		await overviewPage.finish();
+		await expect(page).toHaveURL(/.*\/checkout-complete\.html/);
+		await expect(checkoutCompletePage.thankYouMessage).toHaveText(
+			"Thank you for your order!"
+		);
+	});
+});
 
-    await productPage.validateProductPageIsLoaded()
-    await productPage.addNItems(numOfItems)
-    await expect(productPage.cartBadge).toHaveText(numOfItems.toString());
-})
 
-test('Add 4 items to cart', async ({ page }) => {
-    const numOfItems: number = 4;
-
-    await productPage.validateProductPageIsLoaded()
-    await productPage.addNItems(numOfItems)
-    await expect(productPage.cartBadge).toHaveText(numOfItems.toString());
-})
-
-test('Successfully making a purchase without any item', async ({ page }) => {
-    await productPage.addRandomItems()
-    await productPage.goToCart()
-    await expect(page).toHaveURL(/.*\/cart\.html/);
-    await cartPage.removeFirstItem()
-    await cartPage.checkingOut()
-    await expect(page).toHaveURL(/.*\/checkout-step-one\.html/);
-    await checkoutPage.inputInformation("John", "Maven", "12345")
-    await expect(page).toHaveURL(/.*\/checkout-step-two\.html/);
-    await expect(overviewPage.totalSum).toContainText("$0.00")
-    await overviewPage.finish()
-    await expect(page).toHaveURL(/.*\/checkout-complete\.html/);
-    await expect(checkoutCompletePage.thankYouMessage).toHaveText('Thank you for your order!')
-})
